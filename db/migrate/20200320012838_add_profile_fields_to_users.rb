cdb4fd56528ceaa781c8d82044b9fdb23d4c3b57
Christopher Lam <chrislam67@gmail.com>
2020-05-14 21:17:11 -0500

    remove test migrations, etc

--- db/migrate/20200320012838_add_profile_fields_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddProfileFieldsToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :username, :string
-    add_index :users, :username, unique: true
-    add_column :users, :image, :string
-    add_column :users, :bio, :text
-  end
-end
